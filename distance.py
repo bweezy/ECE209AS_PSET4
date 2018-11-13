6382af66801000267ec9456f6fe4c32812a5f0dc
bweezy <bwood94@ucla.edu>
2018-11-13 14:33:00 -0800

    added initial problem setup

---
+++ distance.py
@@ -0,0 +1,26 @@
+
+import numpy as np
+
+def distance(given, target):
+	x1, y1, theta1 = given
+	x2, y2, theta2 = target
+
+	return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+
+def closest_point(V, target):
+
+	min_distance = float("Inf")
+	closest_point = None
+	for v in V:
+		cur_distance = distance(v, target)
+		if cur_distance < min_distance:
+			min_distance = cur_distance
+			closest_point = v
+
+
+	return closest_point
+
+
+
+
---
+++ obstacle.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+
+class Obstacle:
+
+	def __init__(self, x, y, length, width):
+		self.update(x, y, length, width)
+
+	def update(self, x, y, length, width):
+
+		self.x = x
+		self.y = y
+		self.length = length
+		self.width = width
+		self.border = np.empty((2*(width + length),2))
+		self.fill = np.empty((width*length,2))
+
+		for i in np.arange(width):
+			for j in np.arange(length):
+				self.fill[i*length + j][0] = x + i
+				self.fill[i*length + j][1] = y + j
+
+		for i in np.arange(width):
+			self.border[i][0] = x + i 
+			self.border[i][1] = y
+			self.border[i+1][0] = x + i
+			self.border[i+1][1] = y + length
+
+		for i in np.arange(length):
+			self.border[width + i][0] = x
+			self.border[width + i][1] = y + i
+			self.border[width + i][0] = x + width
+			self.border[width + i][1] = y + i
+
+	def translate(self, r):
+		new_x = min(0, self.x - r)
+		new_y = min(0, self.y - r)
+
+		self.update(new_x, new_y, int(np.ceil(self.length + r)), int(np.ceil(self.width + r)))
+
+
+
+
